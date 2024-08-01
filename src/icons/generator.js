edcae94dd96237d0f9e85f4986722bfacb2f5425
Thomas Mitt <thomas.mitt@hotmail.com>
2024-08-01 17:21:11 -0300

    Initial commit

---
+++ src/icons/generator.js
@@ -0,0 +1,36 @@
+/**
+ * Generate icon.
+ *
+ * @param   parent  Parent element to append icon.
+ * @param   url  Url to redirect (null if func is not null).
+ * @param   func Onclick function for icon (null if url is not null).
+ * @param   svg  SVG as string.
+ * @param   styles  SVG and background colors.
+ * @returns icon as HTML element.
+ */
+function generateIconSocial(parent, url, func, svg, styles) {
+    var icon = $(svg);
+    $(icon).addClass('icon_socials_icon');
+
+    var wrapper;
+
+    if (url != null) {
+        wrapper = $(`<div class="wrapper_socials_icon">
+            <a href="${url}" class="link_socials_icon">
+                ${icon.prop('outerHTML')}
+            </a>
+        </div>`);
+    }
+
+    if (func != null) {
+        wrapper = $(`<div class="wrapper_socials_icon">
+            <span onclick="${func}" class="link_socials_icon">
+                ${icon.prop('outerHTML')}
+            </span>
+        </div>`);
+    }
+
+    $(wrapper).css(styles);
+
+    parent.append(wrapper);
+}
